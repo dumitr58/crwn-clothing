650b2a1b3dff5be4ad8a569cf5486af3a41a2759
dumitr58 <robert@DESKTOP-SAGMNUN.localdomain>
2020-01-10 22:24:23 -0500

    update our menu-items to navigate withRouter HOC

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// this is for our router
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+     document.getElementById('root'));
